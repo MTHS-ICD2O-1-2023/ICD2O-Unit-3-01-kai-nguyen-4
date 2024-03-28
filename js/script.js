3b324726934631ce7f11b89876fc03647dd99464
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-03-28 18:41:12 +0000

    site of trapezoid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function findAreaClicked() {
+  const base1Input = parseInt(document.getElementById("base1-entered").value)
+  const base2Input = parseInt(document.getElementById("base2-entered").value)
+  const heightInput = parseInt(document.getElementById("height-entered").value)
+  const area = ((base1Input * base2Input) / 2) * heightInput
+  document.getElementById("answer").innerHTML = "The area is: " + area.toFixed(2) + " mm<sup>2"
+}
